7a055c911b9a640c64411531b4918cf8fa688aba
Ares9323 <marcopatrignani@hotmail.it>
2021-06-05 14:10:55 +0200

    Suppress logs

--- Source/PuzzlePlatforms/PuzzlePlatformsGameInstance.cpp
+++ Source/PuzzlePlatforms/PuzzlePlatformsGameInstance.cpp
@@ -2,19 +2,19 @@
 
 
 #include "PuzzlePlatformsGameInstance.h"
-//#include "EngineGlobals.h"
+#include "EngineGlobals.h"
 #include "Engine.h"
 
 
 
 UPuzzlePlatformsGameInstance::UPuzzlePlatformsGameInstance(const FObjectInitializer & ObjectInitializer)
 {
-	UE_LOG(LogTemp, Error, TEXT("GameInstance Constructor"));
+	//UE_LOG(LogTemp, Warning, TEXT("GameInstance Constructor"));
 }
 
 void UPuzzlePlatformsGameInstance::Init()
 {
-	UE_LOG(LogTemp, Error, TEXT("GameInstance Init()"));
+	//UE_LOG(LogTemp, Warning, TEXT("GameInstance Init()"));
 }
 
 void UPuzzlePlatformsGameInstance::Host()
@@ -23,12 +23,12 @@ void UPuzzlePlatformsGameInstance::Host()
 	if(!ensure(Engine!=nullptr)) return;
 
 	Engine->AddOnScreenDebugMessage(0,2.f, FColor::Green, TEXT("HOSTING!"));
-	UE_LOG(LogTemp, Error, TEXT("HOSTING"));
+	UE_LOG(LogTemp, Warning, TEXT("HOSTING"));
 
 	UWorld* World = GetWorld();
 	if(!ensure(World!=nullptr)) return;
 
-	World->ServerTravel("/Game/ThirdPersonCPP/Maps/ThirdPersonExampleMap");
+	World->ServerTravel("/Game/ThirdPersonCPP/Maps/ThirdPersonExampleMap?listen");
 }
 
 void UPuzzlePlatformsGameInstance::Join(const FString& Address)
@@ -37,7 +37,7 @@ void UPuzzlePlatformsGameInstance::Join(const FString& Address)
 	if(!ensure(Engine!=nullptr)) return;
 
 	Engine->AddOnScreenDebugMessage(0,5.f, FColor::Green, FString::Printf(TEXT("Joining %s!"), *Address));
-	UE_LOG(LogTemp, Error, TEXT("HOSTING"));
+	UE_LOG(LogTemp, Warning, TEXT("HOSTING"));
 
 	APlayerController* PlayerController = GetFirstLocalPlayerController();
 	if(!ensure(PlayerController!=nullptr)) return;
