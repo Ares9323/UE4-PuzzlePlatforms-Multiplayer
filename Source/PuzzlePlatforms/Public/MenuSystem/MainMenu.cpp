ca7b2696cd1357204d96b2003f8794e5a86c7c9a
Ares9323 <marcopatrignani@hotmail.it>
2021-06-06 14:25:03 +0200

    Bind buttons to functions

--- Source/PuzzlePlatforms/Public/MenuSystem/MainMenu.cpp
+++ Source/PuzzlePlatforms/Public/MenuSystem/MainMenu.cpp
@@ -2,4 +2,33 @@
 
 
 #include "MenuSystem/MainMenu.h"
+#include "Components/Button.h"
 
+bool UMainMenu::Initialize()
+{
+    // Return false if parent function failed
+    bool Success = Super::Initialize();
+    if(!Success) return false;
+
+    // Host OnClick event
+    if(!ensure(Host!=nullptr)) return false;
+    Host->OnClicked.AddDynamic(this, &UMainMenu::HostServer);
+
+    // Host OnClick event
+    if(!ensure(Join!=nullptr)) return false;
+    Join->OnClicked.AddDynamic(this, &UMainMenu::JoinServer);
+
+    return true;
+
+
+}
+
+void UMainMenu::HostServer()
+{
+    UE_LOG(LogTemp, Warning, TEXT("I'm gonna host a server!"));
+}
+
+void UMainMenu::JoinServer()
+{
+    UE_LOG(LogTemp, Warning, TEXT("I'm joining a server!"));
+}
