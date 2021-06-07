a3d3511e8a3d35137731ffa47124b75625746c9c
Ares9323 <marcopatrignani@hotmail.it>
2021-06-08 01:51:03 +0200

    Implemented user count in gamemode

--- Source/PuzzlePlatforms/LobbyGameMode.cpp
+++ Source/PuzzlePlatforms/LobbyGameMode.cpp
@@ -3,3 +3,20 @@
 
 #include "LobbyGameMode.h"
 
+
+void ALobbyGameMode::PostLogin(APlayerController* NewPlayer)
+{
+    int32 PlayersDetected = AGameModeBase::GetNumPlayers();
+    UE_LOG(LogTemp, Error, TEXT("Current players: %d"),PlayersDetected);
+
+    if(PlayersDetected >= 3)
+    {
+        UE_LOG(LogTemp, Warning, TEXT("3 players reached"));
+    }
+}
+
+void ALobbyGameMode::Logout(AController* Exiting)
+{
+    int32 PlayersDetected = AGameModeBase::GetNumPlayers();
+    UE_LOG(LogTemp, Error, TEXT("Current players: %d"),PlayersDetected);
+}
--- Source/PuzzlePlatforms/LobbyGameMode.h
+++ Source/PuzzlePlatforms/LobbyGameMode.h
@@ -7,11 +7,18 @@
 #include "LobbyGameMode.generated.h"
 
 /**
- * 
+ *
  */
 UCLASS()
 class PUZZLEPLATFORMS_API ALobbyGameMode : public APuzzlePlatformsGameMode
 {
 	GENERATED_BODY()
-	
+
+public:
+	void PostLogin(APlayerController* NewPlayer) override;
+
+	void Logout(AController* Exiting) override;
+
+private:
+	uint32 NumberOfPlayers = 0;
 };
