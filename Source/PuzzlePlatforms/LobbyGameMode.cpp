9fe52fa8bcd53f0efce94783f2f8f66d8d2bcec0
Ares9323 <marcopatrignani@hotmail.it>
2021-06-08 01:54:37 +0200

    Calling Super from functions

--- Source/PuzzlePlatforms/LobbyGameMode.cpp
+++ Source/PuzzlePlatforms/LobbyGameMode.cpp
@@ -6,6 +6,7 @@
 
 void ALobbyGameMode::PostLogin(APlayerController* NewPlayer)
 {
+    Super::PostLogin(NewPlayer);
     int32 PlayersDetected = AGameModeBase::GetNumPlayers();
     UE_LOG(LogTemp, Error, TEXT("Current players: %d"),PlayersDetected);
 
@@ -17,6 +18,7 @@ void ALobbyGameMode::PostLogin(APlayerController* NewPlayer)
 
 void ALobbyGameMode::Logout(AController* Exiting)
 {
+    Super::Logout(Exiting);
     int32 PlayersDetected = AGameModeBase::GetNumPlayers();
     UE_LOG(LogTemp, Error, TEXT("Current players: %d"),PlayersDetected);
 }
